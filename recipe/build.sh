3539c1cf7df2c2d1599cb42a91836177dfe7652b
geertsky <geert@verweggistan.eu>
2024-12-23 20:20:48 +0000

    Build against sysroot

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
+
+export CPPFLAGS="--sysroot=$CONDA_BUILD_SYSROOT -I$CONDA_BUILD_SYSROOT/include"
+export LDFLAGS="--sysroot=$CONDA_BUILD_SYSROOT -L$CONDA_BUILD_SYSROOT/lib64"
+
 make prefix=$PREFIX
 make install prefix=$PREFIX
 # remove static library
